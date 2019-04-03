cd5077067f12b59641f116b0ec1a9b44306e8af6
Joey Ferguson <joey.ferguson2@gmail.com>
2019-04-03 15:19:36 -0400

    Small fixes on preliminary HTML rendering

--- gen/BrochureBaseListener.java
+++ gen/BrochureBaseListener.java
@@ -11,204 +11,214 @@ import java.util.ArrayList;
 
 public class BrochureBaseListener implements BrochureListener {
 
-		private BufferedWriter HTMLWriter;
+	private BufferedWriter HTMLWriter;
+	private String CSSFile;
 
-		private ArrayList<String> eventList;
 
-		public BrochureBaseListener(String HTMLPath) {
-			FileWriter fileWriter = getFileWriter(HTMLPath);
-			if (fileWriter != null) {
-				HTMLWriter = new BufferedWriter(fileWriter);
-			}
+	private ArrayList<String> eventList;
 
-			eventList = new ArrayList<>();
+
+	public BrochureBaseListener(String HTMLPath, String CSSPath) {
+		FileWriter fileWriter = getFileWriter(HTMLPath);
+		if (fileWriter != null) {
+			HTMLWriter = new BufferedWriter(fileWriter);
 		}
 
-		private FileWriter getFileWriter(String path) {
-			try {
-				return new FileWriter(path);
-			} catch (IOException e) {
-				e.printStackTrace();
-				return null;
-			}
-		}
+		CSSFile = CSSPath;
 
-		@Override public void enterStart(BrochureParser.StartContext ctx) {
-			try {
-				HTMLWriter.write("<!DOCTYPE html>\n");
-				HTMLWriter.write("<html>\n");
-				HTMLWriter.write("<head>\n");
-				HTMLWriter.write("<meta charset=\"utf-8\">\n");
-				HTMLWriter.write("<link rel='stylesheet' href='' \n");
-				HTMLWriter.write("</head>\n");
-				HTMLWriter.write("<body>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void exitStart(BrochureParser.StartContext ctx) {
-			try {
-				HTMLWriter.write("</body>\n");
-				HTMLWriter.write("</html>\n");
-
-				HTMLWriter.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void enterPage(BrochureParser.PageContext ctx) {
-			try {
-				// front or back
-				HTMLWriter.write("<div id=''>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void exitPage(BrochureParser.PageContext ctx) {
-			try {
-				HTMLWriter.write("</div>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void enterColumn(BrochureParser.ColumnContext ctx) {
-			try {
-				// System.out.println(ctx.getText());
-				// TODO: Figure out how to parse id based on input
-				// left middle right
-				HTMLWriter.write("<div id=''>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void exitColumn(BrochureParser.ColumnContext ctx) {
-			try {
-				HTMLWriter.write("</div>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void enterTitle(BrochureParser.TitleContext ctx) {
-			try {
-				HTMLWriter.write("<h1>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void exitTitle(BrochureParser.TitleContext ctx) {
-			try {
-				HTMLWriter.write("</h1>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void enterHeader(BrochureParser.HeaderContext ctx) {
-			try {
-				HTMLWriter.write("<h2>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void exitHeader(BrochureParser.HeaderContext ctx) {
-			try {
-				HTMLWriter.write("</h2>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void enterFooter(BrochureParser.FooterContext ctx) {
-			try {
-				HTMLWriter.write("<footer>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void exitFooter(BrochureParser.FooterContext ctx) {
-			try {
-				HTMLWriter.write("</footer>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void enterBody(BrochureParser.BodyContext ctx) {
-			try {
-				HTMLWriter.write("<div class='body'>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void exitBody(BrochureParser.BodyContext ctx) {
-			try {
-				HTMLWriter.write("</div>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void enterItem(BrochureParser.ItemContext ctx) {
-			try {
-				HTMLWriter.write("<div>\n");
-				HTMLWriter.write("<p class='date'></p>\n");
-				HTMLWriter.write("<p class='time'></p>\n");
-				HTMLWriter.write("<p class='text'></p>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void exitItem(BrochureParser.ItemContext ctx) {
-			try {
-				HTMLWriter.write("</div>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void enterImage(BrochureParser.ImageContext ctx) {
-			try {
-				HTMLWriter.write("<div>\n");
-				HTMLWriter.write("<img src='' class='img'></img>\n");
-				HTMLWriter.write("<p></p>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void exitImage(BrochureParser.ImageContext ctx) {
-			try {
-				HTMLWriter.write("</div>\n");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override public void enterTitles(BrochureParser.TitlesContext ctx) { }
-		@Override public void exitTitles(BrochureParser.TitlesContext ctx) { }
-		@Override public void enterHeaders(BrochureParser.HeadersContext ctx) { }
-		@Override public void exitHeaders(BrochureParser.HeadersContext ctx) { }
-		@Override public void enterFooters(BrochureParser.FootersContext ctx) { }
-		@Override public void exitFooters(BrochureParser.FootersContext ctx) { }
-		@Override public void enterBodys(BrochureParser.BodysContext ctx) { }
-		@Override public void exitBodys(BrochureParser.BodysContext ctx) { }
-		@Override public void enterItems(BrochureParser.ItemsContext ctx) { }
-		@Override public void exitItems(BrochureParser.ItemsContext ctx) { }
-		@Override public void enterImages(BrochureParser.ImagesContext ctx) { }
-		@Override public void exitImages(BrochureParser.ImagesContext ctx) { }
-		@Override public void enterEveryRule(ParserRuleContext ctx) { }
-		@Override public void exitEveryRule(ParserRuleContext ctx) { }
-		@Override public void visitTerminal(TerminalNode node) { }
-		@Override public void visitErrorNode(ErrorNode node) { }
+		eventList = new ArrayList<>();
 	}
+
+	private FileWriter getFileWriter(String path) {
+		try {
+			return new FileWriter(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	@Override public void enterStart(BrochureParser.StartContext ctx) {
+		try {
+			HTMLWriter.write("<!DOCTYPE html>\n");
+			HTMLWriter.write("<html>\n");
+			HTMLWriter.write("<head>\n");
+			HTMLWriter.write("<title>\n");
+			HTMLWriter.write("Example\n");
+			HTMLWriter.write("</title>\n");
+			HTMLWriter.write("<meta charset=\"utf-8\">\n");
+//			HTMLWriter.write(String.format("<link rel='stylesheet' href='%s'>", CSSFile));
+			HTMLWriter.write("<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css' " +
+							"integrity='sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu' crossorigin='anonymous'>");
+			HTMLWriter.write("</head>\n");
+			HTMLWriter.write("<body>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitStart(BrochureParser.StartContext ctx) {
+		try {
+			HTMLWriter.write("</body>\n");
+			HTMLWriter.write("</html>\n");
+
+			HTMLWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterPage(BrochureParser.PageContext ctx) {
+		try {
+			// front or back
+			HTMLWriter.write("<div id=''>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitPage(BrochureParser.PageContext ctx) {
+		try {
+			HTMLWriter.write("</div>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterColumn(BrochureParser.ColumnContext ctx) {
+		try {
+			// System.out.println(ctx.getText());
+			// TODO: Figure out how to parse id based on input
+			// left middle right
+			HTMLWriter.write("<div id=''>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitColumn(BrochureParser.ColumnContext ctx) {
+		try {
+			HTMLWriter.write("</div>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterTitle(BrochureParser.TitleContext ctx) {
+		try {
+			HTMLWriter.write("<h1>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitTitle(BrochureParser.TitleContext ctx) {
+		try {
+			HTMLWriter.write("</h1>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterHeader(BrochureParser.HeaderContext ctx) {
+		try {
+			HTMLWriter.write("<h2>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitHeader(BrochureParser.HeaderContext ctx) {
+		try {
+			HTMLWriter.write("</h2>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterFooter(BrochureParser.FooterContext ctx) {
+		try {
+			HTMLWriter.write("<footer>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitFooter(BrochureParser.FooterContext ctx) {
+		try {
+			HTMLWriter.write("</footer>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterBody(BrochureParser.BodyContext ctx) {
+		try {
+			HTMLWriter.write("<div class='body'>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitBody(BrochureParser.BodyContext ctx) {
+		try {
+			HTMLWriter.write("</div>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterItem(BrochureParser.ItemContext ctx) {
+		try {
+			HTMLWriter.write("<div>\n");
+			HTMLWriter.write("<p class='date'></p>\n");
+			HTMLWriter.write("<p class='time'></p>\n");
+			HTMLWriter.write("<p class='text'></p>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitItem(BrochureParser.ItemContext ctx) {
+		try {
+			HTMLWriter.write("</div>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterImage(BrochureParser.ImageContext ctx) {
+		try {
+			HTMLWriter.write("<div>\n");
+			HTMLWriter.write("<img src='' class='img'>\n");
+			HTMLWriter.write("<p></p>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitImage(BrochureParser.ImageContext ctx) {
+		try {
+			HTMLWriter.write("</div>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterTitles(BrochureParser.TitlesContext ctx) { }
+	@Override public void exitTitles(BrochureParser.TitlesContext ctx) { }
+	@Override public void enterHeaders(BrochureParser.HeadersContext ctx) { }
+	@Override public void exitHeaders(BrochureParser.HeadersContext ctx) { }
+	@Override public void enterFooters(BrochureParser.FootersContext ctx) { }
+	@Override public void exitFooters(BrochureParser.FootersContext ctx) { }
+	@Override public void enterBodys(BrochureParser.BodysContext ctx) { }
+	@Override public void exitBodys(BrochureParser.BodysContext ctx) { }
+	@Override public void enterItems(BrochureParser.ItemsContext ctx) { }
+	@Override public void exitItems(BrochureParser.ItemsContext ctx) { }
+	@Override public void enterImages(BrochureParser.ImagesContext ctx) { }
+	@Override public void exitImages(BrochureParser.ImagesContext ctx) { }
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	@Override public void visitTerminal(TerminalNode node) { }
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
--- src/BrochureGenerator.java
+++ src/BrochureGenerator.java
@@ -2,13 +2,14 @@ import org.antlr.v4.runtime.*;
 
 public class BrochureGenerator {
     public static void main(String[] args) throws Exception {
-        String inputFile = args[0];
-        String HTMLFile = args[1];
+        String textFileIn  = args[0];
+        String CSSFileIn   = args[1];
+        String HTMLFileOut = args[2];
 
-        CharStream input = CharStreams.fromFileName(inputFile);
+        CharStream input = CharStreams.fromFileName(textFileIn);
         BrochureLexer lexer = new BrochureLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-        BrochureBaseListener listener = new BrochureBaseListener(HTMLFile);
+        BrochureBaseListener listener = new BrochureBaseListener(HTMLFileOut, CSSFileIn);
         BrochureParser parser = new BrochureParser(tokens);
 
         parser.addParseListener(listener);
