e48175b23d1cc511df0ce5b7d5869d0f83bedd49
bshelor <bryson_shelor@taylor.edu>
2019-04-25 14:58:36 -0400

    Revert "Started working on event sorting, can't really figure it out"
    This reverts commit 088f19789d84509d932d2d7ac4d40421c41b0ba3.
    
    Messed up the BaseListener class, think I fixed it all back to the way
    it should be.

--- gen/BrochureBaseListener.java
+++ gen/BrochureBaseListener.java
@@ -10,59 +10,247 @@ import org.antlr.v4.runtime.tree.TerminalNode;
  * of the available methods.
  */
 public class BrochureBaseListener implements BrochureListener {
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterStart(BrochureParser.StartContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitStart(BrochureParser.StartContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterPage1(BrochureParser.Page1Context ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitPage1(BrochureParser.Page1Context ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterPage2(BrochureParser.Page2Context ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitPage2(BrochureParser.Page2Context ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterColumn(BrochureParser.ColumnContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitColumn(BrochureParser.ColumnContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
+
+	private BufferedWriter HTMLWriter;
+	private String CSSFile;
+
+	// Will have to be sorted by date and time for each column, then reset
+	private ArrayList<String> eventList;
+
+
+	public BrochureBaseListener(String HTMLPath, String CSSPath) {
+		FileWriter fileWriter = getFileWriter(HTMLPath);
+		if (fileWriter != null) {
+			HTMLWriter = new BufferedWriter(fileWriter);
+		}
+
+		CSSFile = CSSPath;
+
+		eventList = new ArrayList<>();
+	}
+
+	private FileWriter getFileWriter(String path) {
+		try {
+			return new FileWriter(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	@Override public void enterStart(BrochureParser.StartContext ctx) {
+		try {
+			HTMLWriter.write("<!DOCTYPE html>\n");
+			HTMLWriter.write("<html>\n");
+			HTMLWriter.write("\t<head>\n");
+			HTMLWriter.write("\t\t<title>\n");
+			HTMLWriter.write("\t\t\tExample\n");
+			HTMLWriter.write("\t\t</title>\n");
+			HTMLWriter.write("\t\t<meta charset=\"utf-8\">\n");
+			HTMLWriter.write(String.format("\t\t<link rel='stylesheet' href='%s'>\n", CSSFile));
+			HTMLWriter.write("\t\t<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>\n");
+			HTMLWriter.write("\t</head>\n\n");
+			HTMLWriter.write("\t<body>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitStart(BrochureParser.StartContext ctx) {
+		try {
+			HTMLWriter.write("\t</body>\n");
+			HTMLWriter.write("</html>\n");
+
+			HTMLWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterPage1(BrochureParser.Page1Context ctx) {
+		try {
+			HTMLWriter.write("\t\t<div class='page' id='page1'>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitPage1(BrochureParser.Page1Context ctx) {
+		try {
+			HTMLWriter.write("\t\t</div>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterPage2(BrochureParser.Page2Context ctx) {
+		try {
+			HTMLWriter.write("\t\t<br>\n");
+			HTMLWriter.write("\t\t<div class='page' id='page2'>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitPage2(BrochureParser.Page2Context ctx) {
+		try {
+			HTMLWriter.write("\t\t</div>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterColumn(BrochureParser.ColumnContext ctx) {
+		try {
+			HTMLWriter.write("\t\t\t<div class='column'>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitColumn(BrochureParser.ColumnContext ctx) {
+		try {
+			HTMLWriter.write("\t\t\t</div>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterTitle(BrochureParser.TitleContext ctx) {
+		try {
+			HTMLWriter.write("\t\t\t\t<h1>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitTitle(BrochureParser.TitleContext ctx) {
+		try {
+			String title = ctx.TEXT().getText();
+			title = title.replace("\"", "");
+			HTMLWriter.write("\t\t\t\t\t" + title + "\n");
+			HTMLWriter.write("\t\t\t\t</h1>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterHeader(BrochureParser.HeaderContext ctx) {
+		try {
+			HTMLWriter.write("\t\t\t\t<h2>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitHeader(BrochureParser.HeaderContext ctx) {
+		try {
+			String header = ctx.TEXT().getText();
+			header = header.replace("\"", "");
+			HTMLWriter.write("\t\t\t\t\t" + header + "\n");
+			HTMLWriter.write("\t\t\t\t</h2>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterFooter(BrochureParser.FooterContext ctx) {
+		try {
+			HTMLWriter.write("\t\t\t\t<footer>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitFooter(BrochureParser.FooterContext ctx) {
+		try {
+			String footer = ctx.TEXT().getText();
+			footer = footer.replace("\"", "");
+			HTMLWriter.write("\t\t\t\t\t" + footer + "\n");
+			HTMLWriter.write("\t\t\t\t</footer>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterBody(BrochureParser.BodyContext ctx) {
+		try {
+			HTMLWriter.write("\t\t\t\t<div class='body'>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitBody(BrochureParser.BodyContext ctx) {
+		try {
+			String body = ctx.TEXT().getText();
+			body = body.replace("\"", "");
+			HTMLWriter.write("\t\t\t\t\t" + body + "\n");
+			HTMLWriter.write("\t\t\t\t</div>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterItem(BrochureParser.ItemContext ctx) {
+		try {
+			HTMLWriter.write("\t\t\t\t<div class='item'>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitItem(BrochureParser.ItemContext ctx) {
+		try {
+			HTMLWriter.write("\t\t\t\t\t<p class='date'>\n");
+			String date = ctx.DATE().getText();
+			date = date.replace("\"", "");
+			HTMLWriter.write("\t\t\t\t\t\t" + "Date: " + date + "\n");
+			HTMLWriter.write("\t\t\t\t\t</p>\n");
+
+			HTMLWriter.write("\t\t\t\t\t<p class='time'>\n");
+			String time = ctx.TIME().getText();
+			time = time.replace("\"", "");
+			HTMLWriter.write("\t\t\t\t\t\t" + "Time: " + time + "\n");
+			HTMLWriter.write("\t\t\t\t\t</p>\n");
+
+			HTMLWriter.write("\t\t\t\t\t<p class='text'>\n");
+			String text = ctx.TEXT().getText();
+			text = text.replace("\"", "");
+			HTMLWriter.write("\t\t\t\t\t\t" + text + "\n");
+			HTMLWriter.write("\t\t\t\t\t</p>\n");
+
+			HTMLWriter.write("\t\t\t\t</div>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void enterImage(BrochureParser.ImageContext ctx) {
+		try {
+			HTMLWriter.write("\t\t\t\t<div class='img'>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override public void exitImage(BrochureParser.ImageContext ctx) {
+		try {
+			String text = ctx.TEXT().getText();
+			text = text.replace("\"", "");
+			String url = ctx.URL().getText();
+			url = url.replace("\"", "");
+
+			HTMLWriter.write("\t\t\t\t\t<img src='" + url + "' alt='" + text + "'>\n");
+			HTMLWriter.write("\t\t\t\t\t<p class='caption'>\n");
+			HTMLWriter.write("\t\t\t\t\t\t" + text + "\n");
+			HTMLWriter.write("\t\t\t\t\t</p>\n");
+			HTMLWriter.write("\t\t\t\t</div>\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@Override public void enterTitles(BrochureParser.TitlesContext ctx) { }
 	/**
 	 * {@inheritDoc}
@@ -135,73 +323,6 @@ public class BrochureBaseListener implements BrochureListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterTitle(BrochureParser.TitleContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitTitle(BrochureParser.TitleContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterHeader(BrochureParser.HeaderContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitHeader(BrochureParser.HeaderContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterFooter(BrochureParser.FooterContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitFooter(BrochureParser.FooterContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterBody(BrochureParser.BodyContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitBody(BrochureParser.BodyContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterItem(BrochureParser.ItemContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitItem(BrochureParser.ItemContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterImage(BrochureParser.ImageContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitImage(BrochureParser.ImageContext ctx) { }
 
 	/**
 	 * {@inheritDoc}
